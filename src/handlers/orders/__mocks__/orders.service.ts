edf309a79332ded8736c69a9f4a42819bcb325e6
Carlos Sanchez <casan3@gmail.com>
2022-10-16 14:51:56 -0500

    feat: adding unit tests

---
+++ src/handlers/orders/__mocks__/orders.service.ts
@@ -0,0 +1,5 @@
+export function OrdersService () {
+    return {
+        getOrders: jest.fn(() => [])
+    }
+}
---
+++ src/handlers/orders/orders.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { array } from 'yargs';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+
+jest.mock('./orders.service');
+
+describe('OrdersController', () => {
+  let ordersController: OrdersController;
+  let ordersService: OrdersService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [OrdersController],
+      providers: [OrdersService],
+    }).compile();
+
+    ordersController = app.get<OrdersController>(OrdersController);
+    ordersService = app.get<OrdersService>(OrdersService);
+  });
+
+  describe('Orders controller', () => {
+    it('should be defined', () => {
+      expect(ordersController).toBeDefined();
+    });
+  });
+
+  describe('getOrders', () => {
+    it('should return an array', () => {
+      expect(ordersController.getOrders()).toEqual([]);
+    });
+  });
+});
